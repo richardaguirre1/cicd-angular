c3b90f84319c286fd6e739845d262e10402919b2
Richard Aguirre <richardaguirre1@gmail.com>
2025-02-21 11:45:35 -0300

    add cambio devops en clase test 2

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -10,7 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @RunWith(SpringRunner.class)
 @SpringBootTess
 
-#@ExtendWith(MockitoExtension.class)
+
 public class BasicApplicationTests  {
 
     @Test
