51fbb349fac935d61e143a15a360bb7ecb0123e9
Richard Aguirre <richardaguirre1@gmail.com>
2025-02-21 11:48:19 -0300

    add cambio devops en clase test 4

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -1,21 +1,16 @@
 package com.paymentchain.billing;
 
-import org.junit.Assert;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@RunWith(SpringRunner.class)
-@SpringBootTess
-
-
+@ExtendWith(MockitoExtension.class)
 public class BasicApplicationTests  {
 
     @Test
-    public void contextLoads() {
-       String message = "Default Message cambiado 2025";
-        Assert.assertNotNull(message);
+    void contextLoads() {
+       String message = "Default Message Devops 2025";
+        assertNotNull(message);
     }
 }
